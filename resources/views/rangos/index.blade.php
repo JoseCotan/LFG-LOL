fbced5e72e214a7e1d1fdf591d9dc63c1a4723e9
JoseCotan <joseluis.cotan@iesdonana.org>
2024-04-15 23:40:03 +0200

    Introduciendo nav, footer y diseño

--- resources/views/rangos/index.blade.php
+++ resources/views/rangos/index.blade.php
@@ -1,23 +1,23 @@
 <x-app-layout>
     <div class="relative overflow-x-auto w-full sm:w-5/6 mt-6 mx-auto shadow-md sm:rounded-lg">
         <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-gray-50">
+            <thead class="bg-gray-900 text-white">
                 <tr>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
                         Imagen
                     </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
                         Rango
                     </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
                         Editar rango
                     </th>
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
                         Borrar rango
                     </th>
                 </tr>
             </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
+            <tbody class="bg-gray-800 text-white divide-y divide-gray-200">
                 @foreach ($rangos as $rango)
                 <tr>
                     <td class="px-6 py-4 whitespace-nowrap">
@@ -30,7 +30,7 @@
                         </picture>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap">
-                        <div class="text-sm text-gray-900">{{ $rango->nombre }}</div>
+                        <div class="text-sm text-white">{{ $rango->nombre }}</div>
                     </td>
                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                         <a href="{{ route('rangos.edit', $rango->id) }}" class="text-indigo-600 hover:text-indigo-900 mr-2">Editar</a>
@@ -48,7 +48,7 @@
         </table>
     </div>
     <div class="flex justify-center mt-4 mb-4">
-        <a href="{{ route('rangos.create') }}" class="inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
+        <a href="{{ route('rangos.create') }}" class="inline-flex items-center px-4 py-2 bg-emerald-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:border-indigo-700 focus:ring focus:ring-indigo-200 disabled:opacity-25 transition">
             Añadir Nuevo Rango
         </a>
     </div>
